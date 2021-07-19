e172b74478123635b32a5e7457df6819630e9315
Ester G. Pais <estergpais@hotmail.com>
2021-07-19 18:52:41 -0300

    Update pso.c

--- pso.c
+++ pso.c
@@ -23,8 +23,8 @@ int main (){
 
     const int c1 = 2;         //constante positiva
     const int c2 = 2;
-    const int maxite = 10000;     //total de iterações
-    const int maxrun = 1;     //total de vezes que o programa vai rodar
+    const int maxite = 100000;     //total de iterações
+    const int maxrun = 10;     //total de vezes que o programa vai rodar
 
     int i = 0;
     int j = 0;
