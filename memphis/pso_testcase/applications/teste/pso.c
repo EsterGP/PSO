19c3a79e5b0ba57aa31e3eb3fab46bd34c128239
Ester G. Pais <estergpais@hotmail.com>
2021-08-06 13:52:32 -0300

    Update pso.c

--- memphis/pso_testcase/applications/teste/pso.c
+++ memphis/pso_testcase/applications/teste/pso.c
@@ -4,9 +4,9 @@
 #define N 10   //tamanho da população
 #define M 3     //dimensão de x
 
-void ofun(double x[N][M], double of[]); 
-void min(double f[], double aux[]);
-double pow(double x, double y);
+void ofun(int x[N][M], int of[]); 
+void min(int f[], int aux[]);
+int pow(int x, int y);
 
 int main (){
 	int t;
@@ -29,126 +29,120 @@ int main (){
 	int run = 1;
 	int iteracao = 0;
 
-	double x[N][M];      //população atual
-	double x_ini[N][M];  //população inicial
-	double v[N][M];      //velocidade das partículas
+	int x[N][M];      //população atual
+	int x_ini[N][M];  //população inicial
+	int v[N][M];      //velocidade das partículas
 
-	double pbest[N][M];  //melhor de cada partícula
-	double gbest[M];     //melhor global
-	double f0[N];        //valores de aptidao (fitness)
-	double f[N];         //valores de aptidao (fitness) corrente
+	int pbest[N][M];  //melhor de cada partícula
+	int gbest[M];     //melhor global
+	int f0[N];        //valores de aptidao (fitness)
+	int f[N];         //valores de aptidao (fitness) corrente
 
 	int x_min0, x_min;    //índice dos valores mínimos de fitness
-	double fmin0, fmin;   //minimo guardado e minimo corrente
-	double ffmin[maxite][maxrun];
+	int fmin0, fmin;   //minimo guardado e minimo corrente
+	int ffmin[maxite][maxrun];
 	int ffite[maxrun];
 
-	double aux[2];   //aux[0] guarda o valor mínimo e aux[1] guarda o índice do valor mínimo
+	int aux[2];   //aux[0] guarda o valor mínimo e aux[1] guarda o índice do valor mínimo
 	
 	for(run=1;run<=maxrun;run++){
-    Echo("Comeco da run ");
-        for(i=0;i<N;i++){
-            Echo("Primeiro for");
-            for(j=0;j<M;j++){
-		Echo("formando a populacao inicial");
-                //o máximo que a função rand retorna é 32767 e (double) pq senao ele daria 0 ou 1
-                x_ini[i][j] = lim_inf[j] + ((double)(rand(10,0,32767)/32767)*(lim_sup[j] - lim_inf[j]));
-            }
-        }
-        Echo("Valores iniciais");
-        for(i=0;i<N;i++){
-            for(j=0;j<M;j++){
-                x[i][j] = x_ini[i][j];
-                v[i][j] = 0.1*x_ini[i][j];
-            }
-        }
+		Echo("Comeco da run ");
+		for(i=0;i<N;i++){
+			Echo("Primeiro for");
+			for(j=0;j<M;j++){
+				Echo("formando a populacao inicial");
+				//o máximo que a função rand retorna é 32767 e (double) pq senao ele daria 0 ou 1
+				//x_ini[i][j] = lim_inf[j] + ((double)(rand(10,0,32767)/32767)*(lim_sup[j] - lim_inf[j]));
+			}
+		}
+        	Echo("Valores iniciais");
+        	for(i=0;i<N;i++){
+            		for(j=0;j<M;j++){
+                		x[i][j] = x_ini[i][j];
+				v[i][j] = 0.1*x_ini[i][j];
+            		}
+        	}
 
-        //Calculando a aptidão
-        ofun(x_ini,f0);
+		//Calculando a aptidão
+		ofun(x_ini,f0);
         
-        min(f0,aux);    //pega o indice do menor valor
-        fmin0 = aux[0];
-        x_min0 = aux[1];
+		min(f0,aux);    //pega o indice do menor valor
+		fmin0 = aux[0];
+		x_min0 = aux[1];
 
-        for(i=0;i<N;i++)
-            for(j=0;j<M;j++)
-                pbest[i][j] = x_ini[i][j];
+		for(i=0;i<N;i++)
+			for(j=0;j<M;j++)
+				pbest[i][j] = x_ini[i][j];
 
-        for(j=0;j<M;j++)
-            gbest[j] = x_ini[x_min0][j];
+		for(j=0;j<M;j++)
+			gbest[j] = x_ini[x_min0][j];
 
-        iteracao = 1;
-        tolerancia = 1;
+		iteracao = 1;
 
-        while( (iteracao<=maxite) && (tolerancia > pow(10,-12)) ){
-            w = wmax - (wmax - wmin)*(iteracao/maxite);  //calculando o coeficiente de inércia
+		while(iteracao<=maxite){
+			w = wmax - (wmax - wmin)*(iteracao/maxite);  //calculando o coeficiente de inércia
             
-            //atualizando as velocidades
-            for(i=0;i<N;i++)
-                for(j=0;j<M;j++)
-                    v[i][j] = w*v[i][j] + c1*(rand(t,0,1))*(pbest[i][j]-x[i][j]) + c2*(rand(t,0,1))*(gbest[j]-x[i][j]);
+			//atualizando as velocidades
+			for(i=0;i<N;i++)
+				for(j=0;j<M;j++)
+					v[i][j] = w*v[i][j] + c1*(rand(t,0,1))*(pbest[i][j]-x[i][j]) + c2*(rand(t,0,1))*(gbest[j]-x[i][j]);
             
-            //atualizando a posição das partículas
-            for(i=0;i<N;i++)
-                for(j=0;j<M;j++)
-                    x[i][j] = x[i][j] + v[i][j];
+			//atualizando a posição das partículas
+			for(i=0;i<N;i++)
+				for(j=0;j<M;j++)
+					x[i][j] = x[i][j] + v[i][j];
 
-            //verificando limites
-            for(i=0;i<N;i++){
-                for(j=0;j<M;j++){
-                    if(x[i][j] < lim_inf[j]) x[i][j] = lim_inf[j];
-                    else if(x[i][j] > lim_sup[j]) x[i][j] = lim_sup[j];
-                }
-            }
+			//verificando limites
+			for(i=0;i<N;i++){
+				for(j=0;j<M;j++){
+					if(x[i][j] < lim_inf[j]) x[i][j] = lim_inf[j];
+					else if(x[i][j] > lim_sup[j]) x[i][j] = lim_sup[j];
+                		}
+            		}
 
-            //Calculando a aptidão
-            ofun(x,f);
+            		//Calculando a aptidão
+            		ofun(x,f);
 
-            //atualizando pbest
-            for(i=0;i<N;i++){
-                if(f[i]<f0[i]){
-                    f0[i] = f[i];
-                    for(j=0;j<M;j++) pbest[i][j] = x[i][j];
-                }
-            }
+            		//atualizando pbest
+            		for(i=0;i<N;i++){
+                		if(f[i]<f0[i]){
+                    			f0[i] = f[i];
+                    			for(j=0;j<M;j++) pbest[i][j] = x[i][j];
+                		}
+            		}
 
-            min(f0,aux);
-            fmin = aux[0];
-            x_min = aux[1];
-            ffmin[iteracao][run] = fmin;
-            ffite[run] = iteracao;
+		    	min(f0,aux);
+		    	fmin = aux[0];
+		    	x_min = aux[1];
+		    	ffmin[iteracao][run] = fmin;
+		    	ffite[run] = iteracao;
 
-            if(fmin<fmin0){
-                for(j=0;j<M;j++){
-                    gbest[j] = pbest[x_min][j];
-                    Echo(itoa(gbest[j]));
-                }
-                fmin0 = fmin;
-            }
-
-/*
-            if(iteracao > 100)
-                tolerancia = sqrt(ffmin[iteracao-100][run]-fmin0);
-*/
+            		if(fmin<fmin0){
+                		for(j=0;j<M;j++){
+                    			gbest[j] = pbest[x_min][j];
+                    			Echo(itoa(gbest[j]));
+                		}
+                	fmin0 = fmin;
+            		}
             
-            iteracao++;
-        }
-    }
+            		iteracao++;
+		}
+	}
     
-    for(k=0;k<M;k++)
-        msg.msg[i] = (int)gbest[i];
+    	for(k=0;k<M;k++)
+        	msg.msg[i] = gbest[i];
         
-    msg.length = M;
+    	msg.length = M;
     
-    Send(&msg,resultado);
+    	Send(&msg,resultado);
 
-    Echo("Fitness: ");
-    Echo(itoa(fmin));
-    Echo("Tempo total de execução: ");
-    Echo(GetTick());
+    	Echo("Fitness: ");
+    	Echo(itoa(fmin));
+    	Echo("Tempo total de execução: ");
+    	Echo(GetTick());
 }
 
-void ofun(double x[N][M], double of[]){
+void ofun(int x[N][M], int of[]){
     int i;
 
     for(i=0;i<N;i++){
@@ -157,7 +151,7 @@ void ofun(double x[N][M], double of[]){
     }
 }
 
-void min(double f[],double aux[]){
+void min(int f[],int aux[]){
     int i;
     aux[0] = f[0];
     aux[1] = 0;
@@ -170,9 +164,9 @@ void min(double f[],double aux[]){
     }
 }
 
-double pow(double x, double y){
-    double i;
-    double pot;
+int pow(int x, int y){
+    int i;
+    int pot;
     
     if(y>0){
         pot = x;
